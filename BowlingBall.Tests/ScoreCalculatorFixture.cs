09682335c9ae6eea01b0de48be653d6f36dfd042
odeshpande14 <153592253+odeshpande14@users.noreply.github.com>
2023-12-13 02:21:54 +0530

    added logic for spare bonus

--- BowlingBall.Tests/ScoreCalculatorFixture.cs
+++ BowlingBall.Tests/ScoreCalculatorFixture.cs
@@ -11,20 +11,64 @@ namespace BowlingBall.Tests
         {
             var frames = new List<Frame>
             {
-                new Frame(1, 2),
-                new Frame(1, 2),
-                new Frame(1, 2),
-                new Frame(1, 2),
-                new Frame(1, 2),
-                new Frame(1, 2),
-                new Frame(1, 2),
-                new Frame(1, 2),
-                new Frame(1, 2),
-                new Frame(1, 2),
+                new Frame(1, 2), // 1+2 = 3
+                new Frame(1, 2), // 3+1+2 = 6
+                new Frame(1, 2), // 6+1+2 = 9
+                new Frame(1, 2), // 9+1+2 = 12
+                new Frame(1, 2), // 12+1+2 = 15
+                new Frame(1, 2), // 15+1+2 = 18
+                new Frame(1, 2), // 18+1+2 = 21
+                new Frame(1, 2), // 21+1+2 = 24
+                new Frame(1, 2), // 24+1+2 = 27
+                new Frame(1, 2), // 27+1+2 = 30
             };
             var calculator = new ScoreCalculator();
             int score = calculator.CalculateScore(frames);
             Assert.AreEqual(30, score);
+        }
+
+        [TestMethod]
+        public void CalculateScore_WithSpare_ShouldIncludeSpareBonus_Basic()
+        {
+            var frames = new List<Frame>
+            {
+                new Frame(7, 3), // 7+3+4(B) = 14
+                new Frame(4, 2), // 14+4=2 = 20
+                new Frame(1, 1), // 20+1=1 = 22
+                new Frame(1, 1), // 22+1=1 = 24
+                new Frame(1, 1), // 24+1=1 = 26
+                new Frame(1, 1), // 26+1=1 = 28
+                new Frame(1, 1), // 28+1=1 = 30
+                new Frame(1, 1), // 30+1=1 = 32
+                new Frame(1, 1), // 32+1=1 = 34
+                new Frame(1, 1), // 34+1=1 = 36
+            };
+
+            var calculator = new ScoreCalculator();
+            int score = calculator.CalculateScore(frames);
+            Assert.AreEqual(36, score); 
+        }
+
+        [TestMethod]
+        public void CalculateScore_WithSpare_ShouldIncludeSpareBonus_Multiple()
+        {
+            var frames = new List<Frame>
+            {
+                new Frame(7, 3), // 7+3+4(B) = 14
+                new Frame(4, 2), // 14+4=2 = 20
+                new Frame(1, 1), // 20+1=1 = 22
+                new Frame(1, 1), // 22+1=1 = 24
+                new Frame(6, 4), // 24+6+4+5(B) = 39
+                new Frame(5, 5), // 39+5+5+1(B) = 50
+                new Frame(1, 1), // 50+1+1 = 52
+                new Frame(1, 1), // 52+1+1 = 54
+                new Frame(1, 1), // 54+1+1 = 56
+                new Frame(1, 1), // 56+1+1 = 58
+            };
+
+            var calculator = new ScoreCalculator();
+            int score = calculator.CalculateScore(frames);
+            Assert.AreEqual(58, score);
         }
     }
 }
--- BowlingBall/Services/ScoreCalculator.cs
+++ BowlingBall/Services/ScoreCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using BowlingBall.Enums;
 using BowlingBall.Interfaces;
 using BowlingBall.Models;
 
@@ -9,11 +10,22 @@ namespace BowlingBall.Services
         public int CalculateScore(List<Frame> frames)
         {
             int score = 0;
-            foreach (var frame in frames)
+            for (int i = 0; i < frames.Count; i++)
             {
+                var frame = frames[i];
                 score += frame.FirstRoll + frame.SecondRoll;
+
+                if (frame.Type == FrameType.Spare)
+                {
+                    score += CalculateSpareBonus(frames, i);
+                }
             }
             return score;
+        }
+
+        private static int CalculateSpareBonus(List<Frame> frames, int frameIndex)
+        {
+            return frames[frameIndex + 1].FirstRoll;
         }
     }
 }
