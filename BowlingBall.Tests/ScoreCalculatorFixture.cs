1c097a89329fdc1969d523164ffe51e671ba79b2
odeshpande14 <153592253+odeshpande14@users.noreply.github.com>
2023-12-13 00:56:56 +0530

    added basic score calculator

---
+++ BowlingBall.Tests/ScoreCalculatorFixture.cs
@@ -0,0 +1,31 @@
+﻿using BowlingBall.Models;
+using BowlingBall.Services;
+
+namespace BowlingBall.Tests
+{
+    [TestClass]
+    public class ScoreCalculatorFixture
+    {
+        [TestMethod]
+        public void CalculateScore_WithNormalFrames_ShouldReturnCorrectTotalScore()
+        {
+            var frames = new List<Frame>
+            {
+                new Frame(1, 2),
+                new Frame(1, 2),
+                new Frame(1, 2),
+                new Frame(1, 2),
+                new Frame(1, 2),
+                new Frame(1, 2),
+                new Frame(1, 2),
+                new Frame(1, 2),
+                new Frame(1, 2),
+                new Frame(1, 2),
+            };
+            var calculator = new ScoreCalculator();
+            int score = calculator.CalculateScore(frames);
+            Assert.AreEqual(30, score);
+        }
+    }
+}
+
